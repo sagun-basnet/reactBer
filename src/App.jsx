afbad2275adef23a358de4c3eb69721e0e800921
sagun basnet <basnetsagun8@gmail.com>
2025-03-16 12:59:46 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="flex justify-between h-screen items-center pr-4 ">
+      <div className="border-2 border-black h-[5rem] w-[10rem] rounded-4xl text-red-500 bg-blue-500 hover:bg-red-500 hover:h-[6rem] "></div>
+      <div className="border-2 h-20 w-20"></div>
+      <div className="border-2 h-20 w-20"></div>
+      <div className="border-2 h-20 w-20"></div>
+    </div>
+  );
+};
+
+export default App;
+
+
